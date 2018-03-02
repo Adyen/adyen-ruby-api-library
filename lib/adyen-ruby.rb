876db8af7c43989cd18f7e17cd976ef32ce25fd7
Colin Rood <crrood@gmail.com>
2018-03-02 14:27:07 -0800

    finalize class structure

--- lib/adyen-ruby.rb
+++ lib/adyen-ruby.rb
@@ -8,15 +8,16 @@ module Adyen
 		attr_accessor :ws_user, :ws_pass, :env, :merchant_account
 	end
 
-	def self.api_url_base
+	def self.payment_url_base
 		"https://pal-%s.adyen.com/pal/servlet/Payment/" % @env
+	end
+
+	def self.recurring_url_base
+		"https://pal-%s.adyen.com/pal/servlet/Recurring/" % @env
 	end
 
 	def self.checkout_url_base
 		"https://checkout-%s.adyen.com/services/PaymentSetupAndVerification/" % @env
 	end
 
-	def self.hpp_url_base
-		"https://%s.adyen.com/hpp/" % @env
-	end
 end
