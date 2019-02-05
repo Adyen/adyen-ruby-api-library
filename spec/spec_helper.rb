e0f4517b25214a94a1ee4a809cdd62e9f75639e3
Rik ter Beek <Rik.terBeek@adyen.com>
2019-02-05 09:19:01 +0100

    change ruby gem name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require "webmock/rspec"
 require "base64"
-require_relative "../lib/adyen-ruby-api-client"
+require_relative "../lib/adyen-ruby-api-library"
 
 # disable external connections
 WebMock.disable_net_connect!(allow_localhost: true)
