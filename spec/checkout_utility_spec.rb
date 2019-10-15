0d73b3960fc11e0251f09e72af87b50673e590ac
Damien Burke <damien.burke@voom.flights>
2019-10-14 17:29:21 -0700

    Move origin_keys method from CheckoutUtility service to Checkout service

--- spec/checkout_utility_spec.rb
+++ spec/checkout_utility_spec.rb
@@ -15,13 +15,6 @@ RSpec.describe Adyen::CheckoutUtility, service: "checkout utility" do
     client = Adyen::Client.new(env: :test)
     expect(client.service_url_base(@shared_values[:service])).to eq("https://checkout-test.adyen.com/checkout")
   end
-
-  # must be created manually because every field in the response is an array
-  it "makes an origin_keys call" do
-    parsed_body = create_test(@shared_values[:client], @shared_values[:service], "origin_keys", @shared_values[:client].checkout_utility)
-    expect(parsed_body["originKeys"].class).
-      to be Hash
-  end
 end
 
 # rubocop:enable Metrics/BlockLength
