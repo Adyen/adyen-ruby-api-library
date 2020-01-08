556780688317c2cf924b3787be46036ea5d64545
crrood <crrood@gmail.com>
2020-01-08 13:30:42 -0800

    Mask PCI sensitive data in error logging

--- lib/adyen/errors.rb
+++ lib/adyen/errors.rb
@@ -3,14 +3,33 @@ module Adyen
     attr_reader :code, :response, :request, :msg
 
     def initialize(request = nil, response = nil, msg = nil, code = nil)
+      # mask PCI data in request
+      if not request.nil?
+        request = request.is_a?(Hash) ? request : JSON.parse(request)
+        request.each do |k, v|
+          if request[k].is_a?(Hash)
+            request[k].each do |k2, v2|
+              if k2 == :number
+                v2.replace("#{v2[0,6]}******#{v2[12,16]}")
+              elsif k2 == :cvc
+                v2.replace("*" * v2.length)
+              end
+            end
+          end
+        end
+      end
+
+      # components of formatted error message
       attributes = {
         code: code,
+        msg: msg,
         request: request,
-        response: response,
-        msg: msg
+        response: response
       }.select { |_k, v| v }.map { |k, v| "#{k}:#{v}" }.join(', ')
       message = "#{self.class.name} #{attributes}"
       super(message)
+
+      # internal variables
       @code = code
       @response = response
       @request = request
--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -3,15 +3,45 @@
 require 'spec_helper'
 
 RSpec.describe Adyen::AdyenError do
+  before(:all) do
+    @shared_values = {
+      request: {
+        amount: {
+          currency: "USD",
+          value: 1000
+        },
+        reference: "Your order number",
+        paymentMethod: {
+          type: "scheme",
+          number: "4111111111111111",
+          expiryMonth: "10",
+          expiryYear: "2020",
+          holderName: "John Smith",
+          cvc: "737"
+        },
+        returnUrl: "https://your-company.com/",
+        merchantAccount: "YOUR_MERCHANT_ACCOUNT"
+      }
+    }
+  end
+
   describe '#to_s' do
     it 'describes using the error properties' do
-      expect(Adyen::AdyenError.new('request', 'response', 'message', 'code').to_s).to eq('Adyen::AdyenError code:code, request:request, response:response, msg:message')
+      expect(Adyen::AdyenError.new(@shared_values[:request], 'response', 'message', 'code').to_s).to eq("Adyen::AdyenError code:code, msg:message, request:#{@shared_values[:request]}, response:response")
     end
     it 'skips the null properties' do
-      expect(Adyen::AdyenError.new('request', nil, nil, 'code').to_s).to eq('Adyen::AdyenError code:code, request:request')
+      expect(Adyen::AdyenError.new(@shared_values[:request], nil, nil, 'code').to_s).to eq("Adyen::AdyenError code:code, request:#{@shared_values[:request]}")
     end
     it 'uses the proper error class name' do
-      expect(Adyen::PermissionError.new('a message', 'a request').to_s).to eq('Adyen::PermissionError code:403, request:a request, msg:a message')
+      expect(Adyen::PermissionError.new('message', @shared_values[:request]).to_s).to eq("Adyen::PermissionError code:403, msg:message, request:#{@shared_values[:request]}")
+    end
+  end
+  describe '#masking' do
+    it 'masks card number when logging request in errors' do
+      expect(Adyen::AdyenError.new(@shared_values[:request], 'response', 'message', 'code').request[:paymentMethod][:number]).to eq('411111******1111')
+    end
+    it 'masks CVC when logging request in errors' do
+      expect(Adyen::AdyenError.new(@shared_values[:request], 'response', 'message', 'code').request[:paymentMethod][:cvc]).to eq('***')
     end
   end
 end
