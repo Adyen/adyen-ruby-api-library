67c662e04a3a21805b0709c0a70f22395f04c3c6
alexandros <alexandros.moraitis@adyen.com>
2021-10-12 16:39:11 +0200

    Add 201 status to return

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -358,7 +358,7 @@ RSpec.describe Adyen::Checkout, service: "checkout" do
           "x-api-key" => @shared_values[:client].api_key
         }
       )
-      .to_return(status: 201, body: response_body, headers: {})
+      .to_return(body: response_body, headers: {"status" => 201})
 
     result = @shared_values[:client].checkout.sessions(request_body)
     response_hash = result.response
