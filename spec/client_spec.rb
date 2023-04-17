a8d983400ed782c73b3a3c998ff2fd29b73ea4d6
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-04-17 13:56:12 +0200

    [ITT-111] Generate terminal management api (#131)
    * generate terminal management api
    
    * add url unit test
    
    * Update client.rb

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -177,4 +177,10 @@ RSpec.describe Adyen do
     to eq("https://pal-test.adyen.com/pal/servlet/StoredValue/v46/issue")
   end
 
+  it "checks the creation of PosTerminalManagement url" do
+    client = Adyen::Client.new(api_key: "api_key", env: :test)
+    expect(client.service_url("PosTerminalManagement", "assignTerminals", "1")).
+    to eq("https://postfmapi-test.adyen.com/postfmapi/terminal/v1/assignTerminals")
+  end 
+
 end
