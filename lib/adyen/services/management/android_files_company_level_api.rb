725079c8ef5324e82089608a784d0bde6f21f6c1
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2023-09-11 13:59:04 +0200

    [create-pull-request] automated change (#170)

---
+++ lib/adyen/services/management/android_files_company_level_api.rb
@@ -0,0 +1,38 @@
+require_relative '../service'
+module Adyen
+  class AndroidFilesCompanyLevelApi < Service
+    attr_accessor :service, :version
+
+    def initialize(client, version = DEFAULT_VERSION)
+      super(client, version, 'Management')
+    end
+
+    def list_android_apps(company_id, headers: {}, query_params: {})
+      endpoint = '/companies/{companyId}/androidApps'.gsub(/{.+?}/, '%s')
+      endpoint = endpoint.gsub(%r{^/}, '')
+      endpoint = format(endpoint, company_id)
+      endpoint += create_query_string(query_params)
+      action = { method: 'get', url: endpoint }
+      @client.call_adyen_api(@service, action, {}, headers, @version)
+    end
+
+    def get_android_app(company_id, id, headers: {})
+      endpoint = '/companies/{companyId}/androidApps/{id}'.gsub(/{.+?}/, '%s')
+      endpoint = endpoint.gsub(%r{^/}, '')
+      endpoint = format(endpoint, company_id, id)
+      
+      action = { method: 'get', url: endpoint }
+      @client.call_adyen_api(@service, action, {}, headers, @version)
+    end
+
+    def list_android_certificates(company_id, headers: {}, query_params: {})
+      endpoint = '/companies/{companyId}/androidCertificates'.gsub(/{.+?}/, '%s')
+      endpoint = endpoint.gsub(%r{^/}, '')
+      endpoint = format(endpoint, company_id)
+      endpoint += create_query_string(query_params)
+      action = { method: 'get', url: endpoint }
+      @client.call_adyen_api(@service, action, {}, headers, @version)
+    end
+
+  end
+end
