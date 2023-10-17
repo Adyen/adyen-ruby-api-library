1535d3e3a6c2e25a891fae991917401d63d22cbb
Alessio Zampatti <alessio.zampatti@adyen.com>
2023-10-17 14:01:15 +0000

    adjust disputes tests

--- spec/disputes_spec.rb
+++ spec/disputes_spec.rb
@@ -10,19 +10,10 @@ RSpec.describe Adyen::Disputes, service: 'disputes service' do
 
   # methods / values to test for
   # format is defined in spec_helper
-  test_sets = [
-    ['accept_dispute', 'disputeServiceResult', { 'success' => true }],
-    ['supply_defense_document', 'disputeServiceResult', { 'success' => true }],
-    ['delete_dispute_defense_document', 'disputeServiceResult', { 'success' => true }],
-    ['defend_dispute', 'disputeServiceResult', { 'success' => true }]
-  ]
-
-  generate_tests(client, 'DisputesService', test_sets, client.disputes)
-
   it 'makes a retrieve_applicable_defense_reasons call' do
-    request_body = JSON.parse(json_from_file('mocks/requests/Disputes/retrieve_applicable_defense_reasons.json'))
+    request_body = JSON.parse(json_from_file('mocks/requests/DisputesService/retrieve_applicable_defense_reasons.json'))
 
-    response_body = json_from_file('mocks/responses/Disputes/retrieve_applicable_defense_reasons.json')
+    response_body = json_from_file('mocks/responses/DisputesService/retrieve_applicable_defense_reasons.json')
 
     url = @shared_values[:client].service_url(@shared_values[:service], 'retrieveApplicableDefenseReasons',
                                               @shared_values[:client].disputes.version)
@@ -49,6 +40,6 @@ RSpec.describe Adyen::Disputes, service: 'disputes service' do
     expect(response_hash)
       .to be_a_kind_of Hash
     expect(response_hash['disputeServiceResult']['success'])
-      .to eq('true')
+      .to eq(true)
   end
 end
