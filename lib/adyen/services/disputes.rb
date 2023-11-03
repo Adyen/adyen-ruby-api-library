4b8f5d125b0ad02917260dcff0cfde34e3864a0b
Adyen Automation <plugins_dev@adyen.com>
2023-11-03 10:53:18 +0000

    [create-pull-request] automated change

--- lib/adyen/services/disputes.rb
+++ lib/adyen/services/disputes.rb
@@ -35,15 +35,6 @@ module Adyen
       @client.call_adyen_api(@service, action, request, headers, @version)
     end
 
-    def download_dispute_defense_document(request, headers: {})
-      endpoint = '/downloadDisputeDefenseDocument'.gsub(/{.+?}/, '%s')
-      endpoint = endpoint.gsub(%r{^/}, '')
-      endpoint = format(endpoint)
-      
-      action = { method: 'post', url: endpoint }
-      @client.call_adyen_api(@service, action, request, headers, @version)
-    end
-
     def retrieve_applicable_defense_reasons(request, headers: {})
       endpoint = '/retrieveApplicableDefenseReasons'.gsub(/{.+?}/, '%s')
       endpoint = endpoint.gsub(%r{^/}, '')
