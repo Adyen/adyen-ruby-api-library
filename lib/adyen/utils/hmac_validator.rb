dd2787a6c77a2dfbec78b4a9045762a4002733d1
Matas Zanevičius <matas-zanevicius@users.noreply.github.com>
2024-01-15 17:20:01 +0200

    Refactor/hmac validator (#151)
    * Replace doubles with singles for style
    
    * Bring hmac validator test closer to real life
    
    * Tidy-up hmac validator code
    
    * Backwards compatibility aliasing

--- lib/adyen/utils/hmac_validator.rb
+++ lib/adyen/utils/hmac_validator.rb
@@ -15,7 +15,8 @@ module Adyen
 
       def valid_webhook_hmac?(webhook_request_item, hmac_key)
         expected_sign = calculate_webhook_hmac(webhook_request_item, hmac_key)
-        merchant_sign = fetch(webhook_request_item, 'additionalData.hmacSignature')
+        merchant_sign =
+          webhook_request_item.dig('additionalData', 'hmacSignature')
 
         expected_sign == merchant_sign
       end
@@ -29,29 +30,20 @@ module Adyen
       def calculate_webhook_hmac(webhook_request_item, hmac_key)
         data = data_to_sign(webhook_request_item)
 
-        Base64.strict_encode64(OpenSSL::HMAC.digest(HMAC_ALGORITHM, [hmac_key].pack('H*'), data))
+        Base64.strict_encode64(
+          OpenSSL::HMAC.digest(HMAC_ALGORITHM, [hmac_key].pack('H*'), data)
+        )
       end
 
+      # TODO: Deprecate instead of aliasing
+      alias valid_notification_hmac? valid_webhook_hmac?
+      alias calculate_notification_hmac calculate_webhook_hmac
 
       def data_to_sign(webhook_request_item)
-        data = WEBHOOK_VALIDATION_KEYS.map { |key| fetch(webhook_request_item, key).to_s }
-                                    .join(DATA_SEPARATOR)
-      end
-
-      private
-
-      def fetch(hash, keys)
-        value = hash
-        keys.to_s.split('.').each do |key|
-          value = if key.to_i.to_s == key
-                    value[key.to_i]
-                  else
-                    value[key].nil? ? value[key.to_sym] : value[key]
-                  end
-          break if value.nil?
-        end
-
-        value
+        WEBHOOK_VALIDATION_KEYS
+          .map { webhook_request_item.dig(*_1.split('.')).to_s }
+          .compact
+          .join(DATA_SEPARATOR)
       end
     end
   end
