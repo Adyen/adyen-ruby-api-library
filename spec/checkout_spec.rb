a1d3efa0a5939ba8c3af29bc8c1ee012d411f6dd
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2024-11-05 12:56:51 +0100

    Update all services (#274)
    * false[adyen-sdk-automation] automated change
    
    * removed classic sdk tests
    
    ---------
    
    Co-authored-by: Djoyke Reijans <115019123+DjoykeAbyah@users.noreply.github.com>

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -112,42 +112,6 @@ RSpec.describe Adyen::Checkout, service: 'checkout' do
       .to eq('RedirectShopper')
   end
 
-  # must be created manually due to payments/result format
-  it 'makes a payments/result call' do
-    request_body = JSON.parse(json_from_file('mocks/requests/Checkout/payment-result.json'))
-
-    response_body = json_from_file('mocks/responses/Checkout/payment-result.json')
-
-    url = @shared_values[:client].service_url(@shared_values[:service], 'payments/result',
-                                              @shared_values[:client].checkout.version)
-    WebMock.stub_request(:post, url)
-           .with(
-             body: request_body,
-             headers: {
-               'x-api-key' => @shared_values[:client].api_key
-             }
-           )
-           .to_return(
-             body: response_body
-           )
-
-    result = @shared_values[:client].checkout.classic_checkout_sdk_api.verify_payment_result(request_body)
-    response_hash = result.response
-
-    expect(result.status)
-      .to eq(200)
-    expect(response_hash)
-      .to eq(JSON.parse(response_body))
-    expect(response_hash)
-      .to be_a Adyen::HashWithAccessors
-    expect(response_hash)
-      .to be_a_kind_of Hash
-    expect(response_hash['resultCode'])
-      .to eq('Authorised')
-    expect(response_hash.resultCode)
-      .to eq('Authorised')
-  end
-
   # must be created manually due to paymentsLinks format
   it 'makes a paymentLinks call' do
     request_body = JSON.parse(json_from_file('mocks/requests/Checkout/payment_links.json'))
