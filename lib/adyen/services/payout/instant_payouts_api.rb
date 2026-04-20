0f4d546a182f3c003d961074fbb4540a764db49e
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-04-20 13:39:46 +0000

    [payout] Automated update from Adyen/adyen-openapi@3550ecd

--- lib/adyen/services/payout/instant_payouts_api.rb
+++ lib/adyen/services/payout/instant_payouts_api.rb
@@ -14,9 +14,7 @@ module Adyen
 
     # Make an instant card payout
     def payout(request, headers: {})
-      endpoint = '/payout'.gsub(/{.+?}/, '%s')
-      endpoint = endpoint.gsub(%r{^/}, '')
-      endpoint = format(endpoint)
+      endpoint = build_endpoint('/payout')
       
       action = { method: 'post', url: endpoint }
       @client.call_adyen_api(@service, action, request, headers, @version)
