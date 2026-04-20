53aed55ee6b109788be8196f838b4ad2a204b7ab
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-04-20 13:39:48 +0000

    [binlookup] Automated update from Adyen/adyen-openapi@3550ecd

--- lib/adyen/services/binLookup/bin_lookup_api.rb
+++ lib/adyen/services/binLookup/bin_lookup_api.rb
@@ -14,9 +14,7 @@ module Adyen
 
     # Check if 3D Secure is available
     def get3ds_availability(request, headers: {})
-      endpoint = '/get3dsAvailability'.gsub(/{.+?}/, '%s')
-      endpoint = endpoint.gsub(%r{^/}, '')
-      endpoint = format(endpoint)
+      endpoint = build_endpoint('/get3dsAvailability')
       
       action = { method: 'post', url: endpoint }
       @client.call_adyen_api(@service, action, request, headers, @version)
@@ -24,9 +22,7 @@ module Adyen
 
     # Get a fees cost estimate
     def get_cost_estimate(request, headers: {})
-      endpoint = '/getCostEstimate'.gsub(/{.+?}/, '%s')
-      endpoint = endpoint.gsub(%r{^/}, '')
-      endpoint = format(endpoint)
+      endpoint = build_endpoint('/getCostEstimate')
       
       action = { method: 'post', url: endpoint }
       @client.call_adyen_api(@service, action, request, headers, @version)
