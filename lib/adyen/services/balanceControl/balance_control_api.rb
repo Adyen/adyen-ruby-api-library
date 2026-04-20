c23f76c629ba8ff50bd0c75f9deea47e02182782
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-04-20 13:39:52 +0000

    [balancecontrol] Automated update from Adyen/adyen-openapi@3550ecd

--- lib/adyen/services/balanceControl/balance_control_api.rb
+++ lib/adyen/services/balanceControl/balance_control_api.rb
@@ -16,9 +16,7 @@ module Adyen
     #
     # Deprecated since Adyen Balance Control API v1
     def balance_transfer(request, headers: {})
-      endpoint = '/balanceTransfer'.gsub(/{.+?}/, '%s')
-      endpoint = endpoint.gsub(%r{^/}, '')
-      endpoint = format(endpoint)
+      endpoint = build_endpoint('/balanceTransfer')
       
       action = { method: 'post', url: endpoint }
       @client.call_adyen_api(@service, action, request, headers, @version)
