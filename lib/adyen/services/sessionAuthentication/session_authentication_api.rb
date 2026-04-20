9396792a6cfbb49f80424cba9053e2b53cc82172
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-04-20 13:39:49 +0000

    [sessionauthentication] Automated update from Adyen/adyen-openapi@3550ecd

--- lib/adyen/services/sessionAuthentication/session_authentication_api.rb
+++ lib/adyen/services/sessionAuthentication/session_authentication_api.rb
@@ -14,9 +14,7 @@ module Adyen
 
     # Create a session token
     def create_authentication_session(request, headers: {})
-      endpoint = '/sessions'.gsub(/{.+?}/, '%s')
-      endpoint = endpoint.gsub(%r{^/}, '')
-      endpoint = format(endpoint)
+      endpoint = build_endpoint('/sessions')
       
       action = { method: 'post', url: endpoint }
       @client.call_adyen_api(@service, action, request, headers, @version)
