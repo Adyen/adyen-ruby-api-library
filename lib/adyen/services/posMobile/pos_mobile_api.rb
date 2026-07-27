e0c1931836548c5e72a1619d218901d2661776a9
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-07-27 10:28:53 +0000

    [posmobile] Automated update from Adyen/adyen-openapi@2f7d0e1

--- lib/adyen/services/posMobile/pos_mobile_api.rb
+++ lib/adyen/services/posMobile/pos_mobile_api.rb
@@ -13,6 +13,9 @@ module Adyen
     end
 
     # Create a communication session
+    #
+    # Deprecated since POS Mobile API v68
+    # Use POST [/auth/certificate](https://docs.adyen.com/api-explorer/softpos-configuration-api/latest/post/auth/certificate) to establish secure communications.
     def create_communication_session(request, headers: {})
       endpoint = build_endpoint('/sessions')
       
